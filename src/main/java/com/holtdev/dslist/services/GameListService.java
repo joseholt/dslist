c1a698f3063ed09169a9f0820531b4639accc3f7
Jose Holt <jose_holt@hotmail.com>
2025-08-19 22:02:13 -0300

    GET Lists

---
+++ src/main/java/com/holtdev/dslist/services/GameListService.java
@@ -0,0 +1,31 @@
+package com.holtdev.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.holtdev.dslist.dto.GameListDTO;
+import com.holtdev.dslist.entities.GameList;
+import com.holtdev.dslist.repositories.GameListRepository;
+
+@Service
+public class GameListService {
+
+	@Autowired
+	private GameListRepository gameListRepository;
+	
+	@Transactional(readOnly = true)
+	public List<GameListDTO> findAll(){
+		List<GameList> result = gameListRepository.findAll();
+		return result.stream().map(entity -> new GameListDTO(entity)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public GameListDTO findById(Long id) {
+		GameList result = gameListRepository.findById(id).get();
+		return new GameListDTO(result);
+	}
+	
+}
